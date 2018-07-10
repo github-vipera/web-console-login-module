014e7f333d4dc9d4dad88013273f291415d1e61a
Marco Bonati <marco.bonati@vipera.com>
2018-07-10 10:20:00 +0200

    Update web-console-core to 0.04 with naming refactory

--- projects/web-console-login/src/lib/modules/web-console-login.module.ts
+++ projects/web-console-login/src/lib/modules/web-console-login.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
 import { WebConsoleLoginComponent } from '../components/web-console-login.component';
-import { WebAdminCoreModule } from 'web-console-core'
+import { WebConsoleCoreModule } from 'web-console-core'
 
 @NgModule({
   imports: [
-    FormsModule, WebAdminCoreModule
+    FormsModule, WebConsoleCoreModule
   ],
   declarations: [WebConsoleLoginComponent],
   exports: [WebConsoleLoginComponent]
